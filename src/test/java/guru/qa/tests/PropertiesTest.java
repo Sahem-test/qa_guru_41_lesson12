30494e64ff79b8e905c17c89d9c8c364414b280e
Sahem-test <testroj76@gmail.com>
2026-05-13 10:08:00 +0300

    add some classes

---
+++ src/test/java/guru/qa/tests/PropertiesTest.java
@@ -0,0 +1,13 @@
+package guru.qa.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PropertiesTest {
+
+    @Test
+    void propertyTest(){
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/guru/qa/tests/SkippedTests.java
@@ -0,0 +1,20 @@
+package guru.qa.simple;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class SkippedTests {
+
+    @Test
+    @Disabled
+    void someTest() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled("Some reason")
+    void someTest1() {
+        assertTrue(false);
+    }
+}
