3f355e2fdcae074d7a4dfdaaa96c9cd65b39d494
Sahem-test <testroj76@gmail.com>
2026-05-13 10:42:17 +0300

    add some classes

--- src/test/java/guru/qa/tests/PropertiesTest.java
+++ src/test/java/guru/qa/tests/PropertiesTest.java
@@ -1,5 +1,6 @@
 package guru.qa.tests;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -7,7 +8,19 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class PropertiesTest {
 
     @Test
-    void propertyTest(){
-        assertTrue(true);
+    void propertyTest() {
+        String environment = System.getProperty("environment");
+        System.out.println("Test environment is: " + environment);
+    }
+
+    @Test
+    void propertyNameTest() {
+        String name = System.getProperty("name");
+        System.out.println("My name is: " + name);
+    }
+    @Test
+    void propertyBrowserTest() {
+        String browser = System.getProperty("browser", "crome");
+        System.out.println("Browser is: " + browser);
     }
 }
